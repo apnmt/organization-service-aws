19f263166cce1f22d28895b6c6c1ee88b7e427b0
tobi5775 <tobias.wittmann96@web.de>
2022-04-04 18:39:27 +0200

    change to controller to fix xray subsegment not found

--- src/main/java/de/apnmt/organization/messaging/consumer/OrganizationActivationSqsEventConsumer.java
+++ src/main/java/de/apnmt/organization/messaging/consumer/OrganizationActivationSqsEventConsumer.java
@@ -16,9 +16,9 @@ import io.awspring.cloud.messaging.listener.annotation.SqsListener;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.stereotype.Service;
+import org.springframework.stereotype.Controller;
 
-@Service
+@Controller
 public class OrganizationActivationSqsEventConsumer extends OrganizationActivationEventConsumer {
 
     private static final TypeReference<ApnmtEvent<OrganizationActivationEventDTO>> EVENT_TYPE = new TypeReference<>() {
